920d2bdf233b3b7b70f280f92952c8e3e552fded
iftekharali <ali.iftekhar343@gmail.com>
2018-05-07 10:38:20 +0600

    DataBase Connection Done

---
+++ FirbaseAndroidPanel/Controllers/NotificationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirbaseAndroidPanel.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private WapPortal_CMSEntities _context;
+
+        public NotificationController(WapPortal_CMSEntities context)
+        {
+            _context = context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+          _context.Dispose(); 
+        }
+
+        // GET: Notification
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
