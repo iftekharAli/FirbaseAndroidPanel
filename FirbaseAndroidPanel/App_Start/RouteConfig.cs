9100d08e6d7e7fb9ab8c036265231e6d9790b337
iftekharali <ali.iftekhar343@gmail.com>
2018-05-09 10:05:38 +0600

    WednesDay Start Commit

--- FirbaseAndroidPanel/App_Start/RouteConfig.cs
+++ FirbaseAndroidPanel/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FirbaseAndroidPanel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Notification", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
